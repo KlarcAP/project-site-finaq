c5e417367e0417f560517b08a0ad3598d1025984
makson kawan <makson.costa@ba.estudante.senai.ba>
2024-05-03 19:26:47 -0300

    inclusão do Modal

---
+++ src/components/modal/Modal.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function modal(){
+    return(
+        <div className="Container-Modal"> 
+            <div>
+                <form>
+                    <label>Digite seu nome</label>
+                    <input/>
+                </form>
+                
+            </div>
+        </div>
+    )
+}
